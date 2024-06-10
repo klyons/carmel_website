f53a6e76341e824985d248abc6f59c0bb22edf88
Kelly Franklin-Lyons <lyons.kelly@gmail.com>
2024-06-10 15:50:10 -0700

    Carmel's  yeah
    Signed-off-by: Kelly Franklin-Lyons <lyons.kelly@gmail.com>

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():  # Renamed the function to 'contact'
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
